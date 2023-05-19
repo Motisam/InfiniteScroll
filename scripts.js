ca114ea5aa0b76756364d2e3d84739e3ab6dda2c
moti <motahhareh.samadi@gmail.com>
2023-05-19 19:17:07 +0200

    initial commit

--- scripts.js
+++ scripts.js
@@ -1,1 +1,18 @@
-console.log("Hello World!");
+// getting pictures from unsplash API
+const count = 10;
+const apiKey = 'R91v3HL09tj_a4OUL4tlBsjiZIVGVY2OqbglkDe3kcU';
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+async function getPhotos() {
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// On Load
+
+getPhotos();
